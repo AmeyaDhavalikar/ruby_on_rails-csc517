651dab3d52b343aa765e1bba623248c9c0977dab
sjrappl <sjrappl@ncsu.edu>
2018-10-01 20:15:19 -0400

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :sessions
+
+  root 'users#index'
+
+  get '/', to: 'static_pages#index'
+  get '/index', to: 'static_pages#index'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/test', to: 'sessions#test'
+end
