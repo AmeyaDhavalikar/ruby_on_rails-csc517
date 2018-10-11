5ffb143ee592b26a16f509a3cb4606513969f55d
Sam Rappl <samrappl50@gmail.com>
2018-10-10 22:26:01 -0400

    User access permissions now restricted on houses

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,7 +5,7 @@ class HousesController < ApplicationController
 
   def new
     @house = House.new
-
+    @user = User.find(session[:user_id])
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @house }
@@ -13,38 +13,45 @@ class HousesController < ApplicationController
   end
 
   def create
-    @house = House.new(house_params)
-    @house.realtor_id = session[:user_id]
-    respond_to do |format|
-      if @house.save
-        format.html { redirect_to @house, notice: 'User was successfully created.' }
-        format.json { render json: @house, status: :created, location: @house }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @house.errors, status: :unprocessable_entity }
+    if User.find(session[:user_id]).roles != 2
+      @house = House.new(house_params)
+      @house.realtor_id = session[:user_id]
+      respond_to do |format|
+        if @house.save
+          format.html { redirect_to @house, notice: 'User was successfully created.' }
+          format.json { render json: @house, status: :created, location: @house }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @house.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   def edit
     @house = House.find(params[:id])
+    @user = User.find(session[:user_id])
   end
 
   def update
+    @user = User.find(session[:user_id])
     @house = House.find(params[:id])
+    if @user.roles != 2 && @user.id == @house.realtor_id
 
-    respond_to do |format|
-      if @house.update_attributes(house_params)
-        format.html { redirect_to @house, notice: 'Company was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @house.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @house.update_attributes(house_params)
+          format.html { redirect_to @house, notice: 'Company was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @house.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   def index
+    @user = User.find(session[:user_id])
     @houses = House.all
 
     respond_to do |format|
@@ -64,13 +71,14 @@ class HousesController < ApplicationController
   end
 
   def destroy
-    @house = House.find(params[:id])
-    @house.destroy
+    if User.find(session[:user_id]).roles != 2
+      @house = House.find(params[:id])
+      @house.destroy
 
-    respond_to do |format|
-      format.html { redirect_to houses_url }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to houses_url }
+        format.json { head :no_content }
+      end
     end
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to root_path, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
@@ -49,27 +49,30 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+    if User.find(session[:user_id]).id == :id
+      @user = User.find(params[:id])
 
-    respond_to do |format|
-      if @user.update_attributes(user_params)
-        format.html { redirect_to '/home', notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @user.update_attributes(user_params)
+          format.html { redirect_to '/home', notice: 'User was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+    if User.find(session[:user_id]).id == :id
+      @user = User.find(params[:id])
+      @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to users_url }
+        format.json { head :no_content }
+      end
     end
   end
-
 end
